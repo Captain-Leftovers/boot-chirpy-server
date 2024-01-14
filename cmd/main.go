a93438ee6031844c623a6d6b8b3fae767899e43b
Dobromir Stefanov Ivanov <lalilulelo.mg@gmail.com>
2024-01-14 20:21:38 +1100

    added healthz hanndler

--- cmd/main.go
+++ cmd/main.go
@@ -1,22 +1,29 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
+	"github.com/Captain-Leftovers/boot-chirpy-server/internal/handlers"
 	"github.com/Captain-Leftovers/boot-chirpy-server/internal/middleware"
 )
 
 func main() {
+	filepathRoot := "."
+	port := "3000"
 
 	mux := http.NewServeMux()
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
+	mux.HandleFunc("/healthz", handlers.HealthzHandler)
 
 	corsMux := middleware.MiddlewareCors(mux)
 
-	server := http.Server{
-		Addr:    ":3000",
+	server := &http.Server{
+		Addr:    ":" + port,
 		Handler: corsMux,
 	}
 
-	server.ListenAndServe()
+	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
+	log.Fatal(server.ListenAndServe())
 
 }
